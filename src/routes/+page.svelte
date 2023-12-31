35489332ed30ddd754ef9143066ae02b77efecd1
Frostzeichen <cruztjomar@gmail.com>
2024-01-01 01:24:48 +0800

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    const date = new Date();
+    const year = date.getFullYear();
+</script>
+
+<title>Frostzeichen is here</title>
+<body class="flex gap-10 pl-[5%] pt-1">
+    <nav class="max-w-[200px] pt-20">
+        <p class="text-center text-lg font-bold pl-10 pr-10 pb-5">Navigation</p>
+        <hr class="pb-5">
+        <ul class="grid gap-1">
+            <p>Projects</p>
+            <a href="https://github.com/Frostzeichen/Statistics-in-C" class="pl-5 pr-5 pt-1 pb-1 text-gray-500 hover:bg-gray-200 hover:rounded-sm"><li>Statistics in C</li></a>
+            <a href="https://github.com/Frostzeichen/sensor-server-on-8266" class="pl-5 pr-5 pt-1 pb-1 text-gray-500 hover:bg-gray-200 hover:rounded-sm"><li>Sensor Server on ESP8266</li></a>
+            <!-- <a href="/old-projects" class="pl-5 pr-5 pt-1 pb-1 text-gray-500 hover:bg-gray-200 hover:rounded-sm"><li>Old projects</li></a> -->
+        </ul>
+        <hr class="pb-5">
+        <p class="text-center">
+            Like what you see?<br>
+            View the <a href="/" class="text-gray-500 hover:underline">source code</a>!
+        </p>
+    </nav>
+    <div class="grid gap-10 max-w-[60%] pl-20 pr-[10%]">
+        <h1 class="text-right text-6xl">
+            Terenz Jomar<br>
+            Dela Cruz
+        </h1>
+        <div class="grid gap-5">
+            <p>Call me Terenz. Or Frosty. Or Jelly.</p>
+            <p> I'm a writer, programmer... and a guitarist, if that's relevant.</p>
+            <p>Oh wait! And I also do some citizen science and independent research on growing plants in urban environments.</p>
+            <p>The thing is, I just want to see some really cool stuff. And then make them. This GitHub Page is where I write about them and all the open source projects I have, well, on GitHub. Obviously.</p>
+            <p>That's all. Enjoy!</p>
+        </div>
+        <hr>
+    </div>
+    <footer class="absolute bottom-0">
+        <p>&copy; 2021 - {year}. All rights reserved. Oh and did I say I wrote this in <a href="https://svelte.dev/" class="text-gray-500 hover:underline">Svelte</a>? Because I did.</p>
+    </footer>
+</body>
